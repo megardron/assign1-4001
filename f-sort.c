db55a119399bad1e63c204193416335752c204b0
Megardron <meganardron@cmail.carleton.ca>
2016-02-27 01:16:16 -0500

    adding a file because that's important

---
+++ f-sort.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <semaphore.h>
+
+int *ltrs;
+int *nms;
+
+
+void main() {
+	
+}
